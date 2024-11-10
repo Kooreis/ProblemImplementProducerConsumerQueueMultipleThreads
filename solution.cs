604ed339bc9e37f075f686f5d28326b8eca8334c
Kooreis <kooreis.official@gmail.com>
2024-11-10 13:19:42 -0600

    Added 'enqueue' method to add items to the queue and call the 'process' method. Also added 'process' method to consume items from the queue if it's not empty and not currently processing.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,14 @@
-class Queue {
-    constructor() {
-        this.queue = [];
-        this.isProcessing = false;
+enqueue(item) {
+        this.queue.push(item);
+        this.process();
     }
-}
+
+    async process() {
+        if (this.queue.length > 0 && !this.isProcessing) {
+            this.isProcessing = true;
+            let item = this.queue.shift();
+            await this.consume(item);
+            this.isProcessing = false;
+            this.process();
+        }
+    }
